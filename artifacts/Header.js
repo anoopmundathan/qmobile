c6db2780cdeeeddafc36309f4cdaf635b5c83cc4
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-07-29 22:30:13 +0100

    Add npm build scripts

--- artifacts/Header.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var react_native_1 = require("react-native");
-exports.Header = function () { return React.createElement(react_native_1.View, null, "Header"); };
-//# sourceMappingURL=Header.js.map
